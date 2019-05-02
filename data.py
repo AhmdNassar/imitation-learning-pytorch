65fd9aa3cffac28331408501c7590b4a9faa7c4f
= <engahmdnassar@gmail.com>
2019-05-02 21:39:17 +0200

    Add ToTensor class

--- data.py
+++ data.py
@@ -2,7 +2,6 @@ import torch
 import torchvision
 import numpy as np
 import os
-from torch.utils.data import DataLoader
 from torchvision import transforms
 import h5py as h5
 
@@ -42,4 +41,16 @@ class AgentData:
         return (self.data['rgb'][self.imgIndex],self.data['targets'][self.imgIndex][0],self.data['targets'][self.imgIndex][1], \
                 self.data['targets'][self.imgIndex][2],self.data['targets'][self.imgIndex][10],self.data['targets'][self.imgIndex][24]) 
         
+
+class ToTensor:
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        img = sample
+         
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        img = img.transpose((2, 0, 1))
         
+        return img 
